58e8925b930b3961c3ee4b5416d708af073efe19
Soumay kanti Das <anikds2859@outlook.com>
2021-05-25 17:47:13 +0600

    Add project files.

---
+++ Implicit keyword/Program.cs
@@ -0,0 +1,13 @@
+﻿using system;
+Class MyType
+{
+    public static implicit operator int(MyType m)
+{
+    //code to convert from MyType to int
+}
+}
+MyType X;
+//implicit call MyType's MyType-to-int
+int i = X;
+using System;
+
